ecdc7e0512a87a15ad7503a61171e8fc57862057
Lucas Siqueira Fernandes <lucassiqueirafernandes@MacBook-Air-de-Lucas.local>
2020-06-04 00:16:12 -0300

    setting crud, routes and api

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
 import express, { response } from 'express';
 import routes from './routes';
+import cors from 'cors';
+import path from 'path';
+
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
